1e08fbafc6fe38907a233ef4bc12757e9e83bcbb
yurccow <142795486+yurccow@users.noreply.github.com>
2024-09-23 11:42:03 -0300

    asdasd

--- main.js
+++ main.js
@@ -2,14 +2,16 @@
 
 // alert(nomeUsuario)
 
+console.log("Resultados de um quadrado com 4 lados iguais de 6cm")
+
 function soma(a,b){
     return a+b;
 }
 
-console.log("a soma de dois lados do quadrado é", soma(6,6))
+console.log("a soma de dois lados do quadrado é", soma(6,6), "cm")
 
 function multiplication(a,b){
     return a*b;
 }
 
-console.log("a área do quadrado é", multiplication(6,6))
+console.log("a área do quadrado é", multiplication(6,6), "cm")
