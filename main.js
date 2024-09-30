b20678cea130d05963ee91e6ae07cbebff5215e8
yurccow <142795486+yurccow@users.noreply.github.com>
2024-09-30 11:49:23 -0300

    asd

--- main.js
+++ main.js
@@ -38,7 +38,7 @@
 let estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 let i = 0;
 
-while(i<=27){
+while(i<28){
     console.log(i);
     console.log(estados[i]);
     i = i+1;
