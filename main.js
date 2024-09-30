7c04a80fc0aab80727a8f429417de11eed4f82f1
yurccow <142795486+yurccow@users.noreply.github.com>
2024-09-30 11:19:51 -0300

    asdsd

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
-let nomeUsuario = 'Yuri';
-alert(nomeUsuario)
-alert("fiz sozinho")
+// let nomeUsuario = 'Yuri';
+// alert(nomeUsuario)
+// alert("fiz sozinho")
 
 
-function soma(a,b){
-    return a+b;
-}
+// function soma(a,b){
+//     return a+b;
+// }
 
-console.log("a soma de dois lados do quadrado é", soma(6,6), "cm")
+// console.log("a soma de dois lados do quadrado é", soma(6,6), "cm")
 
-function multiplication(a,b){
-    return a*b;
-}
+// function multiplication(a,b){
+//     return a*b;
+// }
 
-console.log("a área do quadrado é", multiplication(6,6), "cm²")
+// console.log("a área do quadrado é", multiplication(6,6), "cm²")
 
-let areaTriangulo = function(a,b){
-    return (a*b)/2;
-}
+// let areaTriangulo = function(a,b){
+//     return (a*b)/2;
+// }
 
-console.log("a área do triângulo é", areaTriangulo(6,6), "cm²")
+// console.log("a área do triângulo é", areaTriangulo(6,6), "cm²")
 
 
-let a = parseInt(prompt("primeiro valor"))
-let b = parseInt(prompt("segundo valor"))
-let c = parseInt(prompt("terceiro valor"))
+// let a = parseInt(prompt("primeiro valor"))
+// let b = parseInt(prompt("segundo valor"))
+// let c = parseInt(prompt("terceiro valor"))
 
- function media(a,b,c){
-    return ((a+b+c)/3);
+//  function media(a,b,c){
+//     return ((a+b+c)/3);
 
- }
+//  }
 
- document.write(media(a, b, c))
+//  document.write(media(a, b, c))
+
+
+
+let estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
